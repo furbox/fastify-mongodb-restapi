fd7b1976f0a229b7eb7f7835fb655aba09e9c655
Christopher Flores <furbox@gmail.com>
2022-06-22 11:27:50 -0500

    nodemon and get route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const fastify = require('fastify')({
     logger:true
 });
 
+fastify.get("/", (request, reply) => {
+    reply.send({hello:"world"});
+})
+
 const start = async () => {
     await fastify.listen(3000);
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
